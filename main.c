fadfcd6044ea4c03881928cfc03570d6fad41fbc
Loafietheman <jeremihanse8@gmail.com>
2026-04-30 12:31:09 +0700

    CS1_PROGDAS10

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ void vigenereDecrypt(char *ciphertext, char *key) {
 }
 
 int main() {
-    char ciphertext[] = "!gsaH xinH ,fmaskgW rudT"; //Pesan Enkripsi
+    char message[] = "rey txs qb xztwxgnvotB rciS";
     char key[] = "tempkey";
 
     printf("Encypted Text : %s\n", ciphertext);
